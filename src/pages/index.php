7f4912f80813ef04963aab9a2022500ca91a4950
Nico Watine <nico@studioncreations.com>
2016-09-27 20:36:50 -0500

    Awesome splash landing with logo and vid!!!
    Got vertically centered logo image over full screen background gfycat-powered video

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,5 +9,12 @@ body-id: index
 body-class:
 ---
 <div class="bgvid-container">
+	<div class="row absolute-top">
+		<div class="medium-8 medium-push-2 columns end">
+			<div class="logo-wrapper">
+				<img src="/img/logo/white-alpha-bg.png" alt="">
+			</div>
+		</div>
+	</div>
 	<div class='gfyitem' data-id='OccasionalDecisiveKoi'></div>
 </div>
