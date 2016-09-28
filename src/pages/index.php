60d674abb7bc737585364ad0cdcf30df45e0fa07
Nico Watine <nico@studioncreations.com>
2016-09-27 20:14:59 -0500

    Successfully used gfycat to make bg-video
    Full screen ya!
    
    Used custom positioning through the css with specific divs to make it obey my will

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,8 +8,6 @@ og-image:
 body-id: index
 body-class:
 ---
-<div style="position: fixed; z-index: -99; width: 100%; height: 100%">
-	<iframe frameborder="0" height="100%" width="100%" 
-		src="https://youtube.com/embed/NIvicx6v42w?autoplay=1&controls=0&showinfo=0&autohide=1">
-	</iframe>
+<div class="bgvid-container">
+	<div class='gfyitem' data-id='OccasionalDecisiveKoi'></div>
 </div>
