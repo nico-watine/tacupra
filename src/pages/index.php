4ff85369430811cf117af738b71d9e925030c0f9
Nico Watine <nico@studioncreations.com>
2016-09-28 19:32:08 -0500

    More edits to og-info
    Edits both to layout/default and to index page
    
    Worded pretty well, if I do say so myself!
    
    Now just og-image

--- src/pages/index.php
+++ src/pages/index.php
@@ -3,7 +3,7 @@ url: /
 title: Tacoma + Supra Engine Swap
 description: The official site of Team QuattroStig's Tacoma Supra Engine Swap - Toyota Tacupra!
 og-title: Toyota Tacoma + Supra Engine Swap
-og-description: The official site of Team QuattroStig's Tacoma Supra Engine Swap. Follow the build here - Tacupra!
+og-description: The official site of Team QuattroStig's Tacoma Supra Engine Swap. Follow the build here - Go Tacupra!
 og-image:
 body-id: index
 body-class:
