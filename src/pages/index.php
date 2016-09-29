5fb80e9582e211a21e559645c59f50af88e96932
Nico Watine <nico@studioncreations.com>
2016-09-28 19:14:58 -0500

    Added alt to img

--- src/pages/index.php
+++ src/pages/index.php
@@ -11,7 +11,7 @@ body-class:
 <div class="bgvid-container">
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end logo-wrapper">
-			<img src="/img/logo/white-alpha-bg.png" alt="">
+			<img src="/img/logo/white-alpha-bg.png" alt="Toyota Tacupra Swap Logo">
 		</div>
 	</div>
 	<div class='gfyitem' data-id='OccasionalDecisiveKoi'></div>
