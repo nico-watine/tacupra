58f3cf96152f519e2ce0241ddff7a35d094363da
Nico Watine <nico@studioncreations.com>
2016-09-28 19:11:19 -0500

    Adjusted .logo-wrapper styles
    Removed extra layers of divs by making .logo-wrapper actually be the same div as .columns
    
    Then deleted style for .row.absolute-top since absolute-top is no longer needed

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,11 +9,9 @@ body-id: index
 body-class:
 ---
 <div class="bgvid-container">
-	<div class="row absolute-top">
-		<div class="medium-8 medium-push-2 columns end">
-			<div class="logo-wrapper">
-				<img src="/img/logo/white-alpha-bg.png" alt="">
-			</div>
+	<div class="row">
+		<div class="medium-8 medium-push-2 columns end logo-wrapper">
+			<img src="/img/logo/white-alpha-bg.png" alt="">
 		</div>
 	</div>
 	<div class='gfyitem' data-id='OccasionalDecisiveKoi'></div>
