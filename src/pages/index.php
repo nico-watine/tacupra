1edae7d72b0a260960eb90dd640c03c47749c08f
Nico Watine <nico@studioncreations.com>
2017-06-10 11:43:53 -0500

    Add equalizer markup in index for specCard element
    Functioning well, merge and close

--- src/pages/index.php
+++ src/pages/index.php
@@ -23,9 +23,9 @@ layout: index-video-splash
 			<h1><strong>Build Specs</strong></h1>
 		</div>
 	</div>
-	<div class="row">
+	<div class="row" data-equalizer>
 		<div class="medium-6 columns">
-			<div class="specCard">
+			<div class="specCard" data-equalizer-watch>
 				<div class="specCard-header">
 					<h2><strong>Build Truck</strong></h2>
 					<div class="image-scrim">&nbsp;</div>
@@ -40,7 +40,7 @@ layout: index-video-splash
 			</div>
 		</div>
 		<div class="medium-6 columns">
-			<div class="specCard">
+			<div class="specCard" data-equalizer-watch>
 				<div class="specCard-header">
 					<h2><strong>Swap Engine</strong></h2>
 					<div class="image-scrim">&nbsp;</div>
