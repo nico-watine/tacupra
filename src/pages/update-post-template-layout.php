a751f9b7c1ab5202ca0896045f57bf3053f936c7
Nico Watine <nico@studioncreations.com>
2017-06-13 16:25:37 -0500

    Header structure for hero img and absolute h1

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -6,6 +6,14 @@ og-title:
 og-description: 
 og-image: 
 body-id: 
-body-class:
+body-class: update-post
 layout: 
 ---
+<header>
+	<div class="hero">
+		<div class="prose text-center">
+			<h1><strong>2001 Toyota Tacoma Suspension</strong></h1>
+		</div>
+		<img src="/img/update-post/template-layout/hero-max.jpg" alt="">
+	</div>
+</header>
