266a7499fae1f28068504543bce55e598f9c1da8
Nico Watine <nico@studioncreations.com>
2017-06-13 21:31:53 -0500

    Updated entire page with original post content

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -35,4 +35,85 @@ layout:
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/rq8U3EF.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Upper ball joints pressed out of the spindle. </p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/FBWi942.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/KX48uLp.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/XJ872rX.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/S9FdPh8.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Mocking up the Camburg upper control arms. </p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/kPPrd3F.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Close shot of the mock-up.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/bnzwzTW.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/3B9fH8r.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>First time welding Mig uphill. <br>
+			See settings here: <a href="http://imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
+			I was running a generous amount of 75/25 Argon CO2</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/hLk01as.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Prepping for paint.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/XfO1qIk.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/Zn2s1XG.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>I really love the way it turned out. Now all I have to do is get an alignment and tune the valving. Will follow up with video when I can get the truck out.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/lUZomrF.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>One off Deaver Leaf Springs - Specific to my application. <br>
+			Increase payload capacity 600 Lbs with minimal lift.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/H1kW5sv.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<p>Increased travel!</p>
+		</div>
+	</div>
 </article>
