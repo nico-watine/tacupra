281eb55a7e1b85e32275d99a3aa95659742d4990
Nico Watine <nico@studioncreations.com>
2017-06-13 21:23:34 -0500

    Create red section, sans supra logo

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -27,4 +27,12 @@ layout:
 			<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
 		</div>
 	</div>
+	<div class="red-container" style="padding: 30px 0; margin-bottom: 30px; background: #BB3938;">
+		<div class="row">
+			<div class="large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/2idX8KI.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+				<p style="font-weight: 500; color: #fff;">Pressing out ball joints with OTC press (Part# 7249)  and Toyota specific ball joint press adapter (Part# 6733). I was unimpressed by the fact that in combination with the relief cup and the Toyota specific adapter the tool could only get one thread to start. But, it did end up working (Just sketchy)</p>
+			</div>
+		</div>
+	</div>
 </article>
