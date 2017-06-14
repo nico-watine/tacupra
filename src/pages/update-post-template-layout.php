100fd63bc9b8343ad1e3849e484a2780a7b50046
Nico Watine <nico@studioncreations.com>
2017-06-13 21:17:11 -0500

    Inline-style first image section

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -9,7 +9,7 @@ body-id:
 body-class: update-post
 layout: 
 ---
-<header>
+<header style="margin-bottom: 30px;">
 	<div class="hero">
 		<div class="prose text-center">
 			<h1><strong>2001 Toyota Tacoma Suspension</strong></h1>
@@ -19,3 +19,12 @@ layout:
 		<img src="/img/update-post/template-layout/hero-max.jpg" alt="">
 	</div>
 </header>
+<article>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<img src="http://i.imgur.com/wXh51Wo.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin-bottom: 20px;" alt="">
+			<h5><strong>Stock setup (with Tundra Brakes)</strong></h5>
+			<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
+		</div>
+	</div>
+</article>
