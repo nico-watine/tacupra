4de92a4e0a2d9193d8b7e11826539d7e71f13b39
Nico Watine <nico@studioncreations.com>
2017-06-14 13:26:13 -0500

    Wrap every .row in a .container

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -20,11 +20,13 @@ layout:
 	</div>
 </header>
 <article>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/wXh51Wo.jpg" alt="">
-			<h5><strong>Stock setup (with Tundra Brakes)</strong></h5>
-			<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/wXh51Wo.jpg" alt="">
+				<h5><strong>Stock setup (with Tundra Brakes)</strong></h5>
+				<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
+			</div>
 		</div>
 	</div>
 	<div class="red-container" style="background: #BB3938;">
@@ -35,7 +37,7 @@ layout:
 			</div>
 		</div>
 	</div>
-	<div class="container" style="background: #fff;">
+	<div class="container">
 		<div class="row">
 			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
 				<img src="http://i.imgur.com/rq8U3EF.jpg" alt="">
@@ -43,79 +45,103 @@ layout:
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/FBWi942.jpg" alt="">
-			<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/FBWi942.jpg" alt="">
+				<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/KX48uLp.jpg" alt="">
-			<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/KX48uLp.jpg" alt="">
+				<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/XJ872rX.jpg" alt="">
-			<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/XJ872rX.jpg" alt="">
+				<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/S9FdPh8.jpg" alt="">
-			<p>Mocking up the Camburg upper control arms. </p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/S9FdPh8.jpg" alt="">
+				<p>Mocking up the Camburg upper control arms. </p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/kPPrd3F.jpg" alt="">
-			<p>Close shot of the mock-up.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/kPPrd3F.jpg" alt="">
+				<p>Close shot of the mock-up.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/bnzwzTW.jpg" alt="">
-			<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/bnzwzTW.jpg" alt="">
+				<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/3B9fH8r.jpg" alt="">
-			<p>First time welding Mig uphill. <br>
-			See settings here: <a href="http://imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
-			I was running a generous amount of 75/25 Argon CO2</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/3B9fH8r.jpg" alt="">
+				<p>First time welding Mig uphill. <br>
+				See settings here: <a href="http://imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
+				I was running a generous amount of 75/25 Argon CO2</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/hLk01as.jpg" alt="">
-			<p>Prepping for paint.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/hLk01as.jpg" alt="">
+				<p>Prepping for paint.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/XfO1qIk.jpg" alt="">
-			<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/XfO1qIk.jpg" alt="">
+				<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/Zn2s1XG.jpg" alt="">
-			<p>I really love the way it turned out. Now all I have to do is get an alignment and tune the valving. Will follow up with video when I can get the truck out.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/Zn2s1XG.jpg" alt="">
+				<p>I really love the way it turned out. Now all I have to do is get an alignment and tune the valving. Will follow up with video when I can get the truck out.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/lUZomrF.jpg" alt="">
-			<p>One off Deaver Leaf Springs - Specific to my application. <br>
-			Increase payload capacity 600 Lbs with minimal lift.</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/lUZomrF.jpg" alt="">
+				<p>One off Deaver Leaf Springs - Specific to my application. <br>
+				Increase payload capacity 600 Lbs with minimal lift.</p>
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/H1kW5sv.jpg" alt="">
-			<p>Increased travel!</p>
+	<div class="container">
+		<div class="row">
+			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+				<img src="http://i.imgur.com/H1kW5sv.jpg" alt="">
+				<p>Increased travel!</p>
+			</div>
 		</div>
 	</div>
 </article>
