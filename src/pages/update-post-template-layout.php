ae0a379f7766992fd7b609195008b0a4f2838087
Nico Watine <nico@studioncreations.com>
2017-06-14 13:54:31 -0500

    All .row .columns converted to .content

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -31,7 +31,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/2idX8KI.jpg" alt="">
 				<p>Pressing out ball joints with OTC press (Part# 7249)  and Toyota specific ball joint press adapter (Part# 6733). I was unimpressed by the fact that in combination with the relief cup and the Toyota specific adapter the tool could only get one thread to start. But, it did end up working (Just sketchy)</p>
 			</div>
@@ -39,7 +39,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/rq8U3EF.jpg" alt="">
 				<p>Upper ball joints pressed out of the spindle. </p>
 			</div>
@@ -47,7 +47,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/FBWi942.jpg" alt="">
 				<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
 			</div>
@@ -55,7 +55,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/KX48uLp.jpg" alt="">
 				<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
 			</div>
@@ -63,7 +63,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/XJ872rX.jpg" alt="">
 				<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
 			</div>
@@ -71,7 +71,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/S9FdPh8.jpg" alt="">
 				<p>Mocking up the Camburg upper control arms. </p>
 			</div>
@@ -79,7 +79,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/kPPrd3F.jpg" alt="">
 				<p>Close shot of the mock-up.</p>
 			</div>
@@ -87,7 +87,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/bnzwzTW.jpg" alt="">
 				<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
 			</div>
@@ -95,7 +95,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/3B9fH8r.jpg" alt="">
 				<p>First time welding Mig uphill. <br>
 				See settings here: <a href="http://imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
@@ -105,7 +105,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/hLk01as.jpg" alt="">
 				<p>Prepping for paint.</p>
 			</div>
@@ -113,7 +113,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/XfO1qIk.jpg" alt="">
 				<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
 			</div>
@@ -121,7 +121,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/Zn2s1XG.jpg" alt="">
 				<p>I really love the way it turned out. Now all I have to do is get an alignment and tune the valving. Will follow up with video when I can get the truck out.</p>
 			</div>
@@ -129,7 +129,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/lUZomrF.jpg" alt="">
 				<p>One off Deaver Leaf Springs - Specific to my application. <br>
 				Increase payload capacity 600 Lbs with minimal lift.</p>
@@ -138,7 +138,7 @@ layout:
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="medium-10 medium-push-1 large-8 large-push-2 columns end">
+			<div class="content">
 				<img src="http://i.imgur.com/H1kW5sv.jpg" alt="">
 				<p>Increased travel!</p>
 			</div>
