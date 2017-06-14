b9bb62622fc4877b4656b2772e18a1c7021bcf80
Nico Watine <nico@studioncreations.com>
2017-06-14 13:09:07 -0500

    All inline-styles moved to sass
    Excluding the one .red-section

--- src/pages/update-post-template-layout.php
+++ src/pages/update-post-template-layout.php
@@ -20,99 +20,99 @@ layout:
 	</div>
 </header>
 <article>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/wXh51Wo.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/wXh51Wo.jpg" alt="">
 			<h5><strong>Stock setup (with Tundra Brakes)</strong></h5>
 			<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
 		</div>
 	</div>
 	<div class="red-container" style="background: #BB3938;">
-		<div class="row" style="padding: 16px 0;">
+		<div class="row">
 			<div class="large-8 large-push-2 columns end">
-				<img src="http://i.imgur.com/2idX8KI.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+				<img src="http://i.imgur.com/2idX8KI.jpg" alt="">
 				<p style="font-weight: 500; color: #fff;">Pressing out ball joints with OTC press (Part# 7249)  and Toyota specific ball joint press adapter (Part# 6733). I was unimpressed by the fact that in combination with the relief cup and the Toyota specific adapter the tool could only get one thread to start. But, it did end up working (Just sketchy)</p>
 			</div>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/rq8U3EF.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/rq8U3EF.jpg" alt="">
 			<p>Upper ball joints pressed out of the spindle. </p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/FBWi942.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/FBWi942.jpg" alt="">
 			<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/KX48uLp.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/KX48uLp.jpg" alt="">
 			<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/XJ872rX.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/XJ872rX.jpg" alt="">
 			<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/S9FdPh8.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/S9FdPh8.jpg" alt="">
 			<p>Mocking up the Camburg upper control arms. </p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/kPPrd3F.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/kPPrd3F.jpg" alt="">
 			<p>Close shot of the mock-up.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/bnzwzTW.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/bnzwzTW.jpg" alt="">
 			<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/3B9fH8r.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/3B9fH8r.jpg" alt="">
 			<p>First time welding Mig uphill. <br>
 			See settings here: <a href="http://imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
 			I was running a generous amount of 75/25 Argon CO2</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/hLk01as.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/hLk01as.jpg" alt="">
 			<p>Prepping for paint.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/XfO1qIk.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/XfO1qIk.jpg" alt="">
 			<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/Zn2s1XG.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/Zn2s1XG.jpg" alt="">
 			<p>I really love the way it turned out. Now all I have to do is get an alignment and tune the valving. Will follow up with video when I can get the truck out.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/lUZomrF.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/lUZomrF.jpg" alt="">
 			<p>One off Deaver Leaf Springs - Specific to my application. <br>
 			Increase payload capacity 600 Lbs with minimal lift.</p>
 		</div>
 	</div>
-	<div class="row" style="padding: 16px 0;">
+	<div class="row">
 		<div class="large-8 large-push-2 columns end">
-			<img src="http://i.imgur.com/H1kW5sv.jpg" style="border: 3px solid #FFFFFF; box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37); margin: 16px 0;" alt="">
+			<img src="http://i.imgur.com/H1kW5sv.jpg" alt="">
 			<p>Increased travel!</p>
 		</div>
 	</div>
