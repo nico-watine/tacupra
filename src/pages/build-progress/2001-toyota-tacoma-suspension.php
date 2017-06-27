e42066c5178771c8a44a162cc15c0e0c759b0dfc
Nico Watine <nico@studioncreations.com>
2017-06-27 13:02:35 -0500

    Add alt values to all images in the post

--- src/pages/build-progress/2001-toyota-tacoma-suspension.php
+++ src/pages/build-progress/2001-toyota-tacoma-suspension.php
@@ -15,7 +15,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/wXh51Wo.jpg" alt="">
+			<img src="http://i.imgur.com/wXh51Wo.jpg" alt="Tacupra Update Post Image">
 			<h5><strong>Stock setup (with Tundra Brakes)</strong></h5>
 			<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
 		</div>
@@ -24,7 +24,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/2idX8KI.jpg" alt="">
+			<img src="http://i.imgur.com/2idX8KI.jpg" alt="Tacupra Update Post Image">
 			<p>Pressing out ball joints with OTC press (Part# 7249)  and Toyota specific ball joint press adapter (Part# 6733). I was unimpressed by the fact that in combination with the relief cup and the Toyota specific adapter the tool could only get one thread to start. But, it did end up working (Just sketchy)</p>
 		</div>
 	</div>
@@ -32,7 +32,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/rq8U3EF.jpg" alt="">
+			<img src="http://i.imgur.com/rq8U3EF.jpg" alt="Tacupra Update Post Image">
 			<p>Upper ball joints pressed out of the spindle. </p>
 		</div>
 	</div>
@@ -40,7 +40,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/FBWi942.jpg" alt="">
+			<img src="http://i.imgur.com/FBWi942.jpg" alt="Tacupra Update Post Image">
 			<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
 		</div>
 	</div>
@@ -48,7 +48,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/KX48uLp.jpg" alt="">
+			<img src="http://i.imgur.com/KX48uLp.jpg" alt="Tacupra Update Post Image">
 			<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
 		</div>
 	</div>
@@ -56,7 +56,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/XJ872rX.jpg" alt="">
+			<img src="http://i.imgur.com/XJ872rX.jpg" alt="Tacupra Update Post Image">
 			<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
 		</div>
 	</div>
@@ -64,7 +64,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/S9FdPh8.jpg" alt="">
+			<img src="http://i.imgur.com/S9FdPh8.jpg" alt="Tacupra Update Post Image">
 			<p>Mocking up the Camburg upper control arms. </p>
 		</div>
 	</div>
@@ -72,7 +72,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/kPPrd3F.jpg" alt="">
+			<img src="http://i.imgur.com/kPPrd3F.jpg" alt="Tacupra Update Post Image">
 			<p>Close shot of the mock-up.</p>
 		</div>
 	</div>
@@ -80,7 +80,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/bnzwzTW.jpg" alt="">
+			<img src="http://i.imgur.com/bnzwzTW.jpg" alt="Tacupra Update Post Image">
 			<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
 		</div>
 	</div>
@@ -88,7 +88,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/3B9fH8r.jpg" alt="">
+			<img src="http://i.imgur.com/3B9fH8r.jpg" alt="Tacupra Update Post Image">
 			<p>First time welding Mig uphill. <br>
 			See settings here: <a href="http://imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
 			I was running a generous amount of 75/25 Argon CO2</p>
@@ -98,7 +98,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/hLk01as.jpg" alt="">
+			<img src="http://i.imgur.com/hLk01as.jpg" alt="Tacupra Update Post Image">
 			<p>Prepping for paint.</p>
 		</div>
 	</div>
@@ -106,7 +106,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/XfO1qIk.jpg" alt="">
+			<img src="http://i.imgur.com/XfO1qIk.jpg" alt="Tacupra Update Post Image">
 			<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
 		</div>
 	</div>
@@ -114,7 +114,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/Zn2s1XG.jpg" alt="">
+			<img src="http://i.imgur.com/Zn2s1XG.jpg" alt="Tacupra Update Post Image">
 			<p>I really love the way it turned out. Now all I have to do is get an alignment and tune the valving. Will follow up with video when I can get the truck out.</p>
 		</div>
 	</div>
@@ -122,7 +122,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/lUZomrF.jpg" alt="">
+			<img src="http://i.imgur.com/lUZomrF.jpg" alt="Tacupra Update Post Image">
 			<p>One off Deaver Leaf Springs - Specific to my application. <br>
 			Increase payload capacity 600 Lbs with minimal lift.</p>
 		</div>
@@ -131,7 +131,7 @@ upload-date: Uploaded March 18 2016
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/H1kW5sv.jpg" alt="">
+			<img src="http://i.imgur.com/H1kW5sv.jpg" alt="Tacupra Update Post Image">
 			<p>Increased travel!</p>
 		</div>
 	</div>
