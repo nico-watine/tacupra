de3b40f6d2d3daf41327d3fda3bd38d7b8a6c0de
Nico Watine <nico@studioncreations.com>
2017-06-29 01:39:02 -0500

    Remove the id from the cards

--- src/pages/index.php
+++ src/pages/index.php
@@ -81,7 +81,7 @@ layout: index-video-splash
 <section class="updatePost-links">
 	<div class="row expanded" data-equalizer>
 		<div class="medium-8 medium-push-2 large-4 large-push-0 columns">
-			<div class="postLink-card" id="style-2" data-equalizer-watch>
+			<div class="postLink-card" data-equalizer-watch>
 				<img class="feature-image" src="{{ cdn }}/img/update-post/2001-toyota-tacoma-suspension/post-thumb.jpg" alt="Update Post Thumbnail">
 				<div class="prose">
 					<h4 class="post-title"><strong>Suspension &amp; Brakes</strong></h4>
@@ -90,7 +90,7 @@ layout: index-video-splash
 			</div>
 		</div>
 		<div class="medium-8 medium-push-2 large-4 large-push-0 columns">
-			<div class="postLink-card" id="style-2" data-equalizer-watch>
+			<div class="postLink-card" data-equalizer-watch>
 				<img class="feature-image" src="{{ cdn }}/img/update-post/2001-toyota-tacoma-suspension/post-thumb.jpg" alt="Update Post Thumbnail">
 				<div class="prose">
 					<h4 class="post-title"><strong>Suspension &amp; Brakes</strong></h4>
@@ -99,7 +99,7 @@ layout: index-video-splash
 			</div>
 		</div>
 		<div class="medium-8 medium-push-2 large-4 large-push-0 end columns">
-			<div class="postLink-card" id="style-2" data-equalizer-watch>
+			<div class="postLink-card" data-equalizer-watch>
 				<img class="feature-image" src="{{ cdn }}/img/update-post/2001-toyota-tacoma-suspension/post-thumb.jpg" alt="Update Post Thumbnail">
 				<div class="prose">
 					<h4 class="post-title"><strong>Suspension &amp; Brakes</strong></h4>
