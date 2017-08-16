a8222d460f71a80ff93b9c9daf6678705d05ee76
Nico Watine <nico@studioncreations.com>
2017-08-16 17:44:40 -0500

    Copy/paste bulk of post content
    Need to css video embeds. When that is done, can complete the post

--- src/pages/build-progress/single-turbo-fitment.php
+++ src/pages/build-progress/single-turbo-fitment.php
@@ -15,9 +15,8 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/wXh51Wo.jpg" alt="Tacupra Update Post Image">
-			<h5><strong>Stock setup (with Tundra Brakes)</strong></h5>
-			<p>In effort to minimize distracting watermarks I have put urls at the bottom of each image description. Please excuse its repetitive nature, I have just been experiencing issues where only a single image is shared in which there is no reference.</p>
+			<img src="http://i.imgur.com/9zJQdRw.jpg" alt="Tacupra Update Post Image">
+			<p>The R154 bellhousing was bolted to the R150F and the transmission crossmember was modified (pushing the full engine/trans assembly back 2-3.4"). The water pump pulley is VERY close to the radiator.</p>
 		</div>
 	</div>
 </div>
@@ -25,39 +24,41 @@ upload-date: Uploaded June 10 2017
 	<div class="row">
 		<div class="content">
 			<img src="//i.imgur.com/2idX8KI.jpg" alt="Tacupra Update Post Image">
-			<p>Pressing out ball joints with OTC press (Part# 7249)  and Toyota specific ball joint press adapter (Part# 6733). I was unimpressed by the fact that in combination with the relief cup and the Toyota specific adapter the tool could only get one thread to start. But, it did end up working (Just sketchy)</p>
+			<p>The stock twins will not fit for this application without cutting into the firewall and relocating critical A/C components. The rear turbo is right up against the firewall. Then the hard piping extends even further for the inlet. The engine could not move further forward without moving the radiator/core support/front bumper forward. <br>
+			This is when I decided that the single turbo was my plan of action.</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/rq8U3EF.jpg" alt="Tacupra Update Post Image">
-			<p>Upper ball joints pressed out of the spindle. </p>
+			<img src="http://i.imgur.com/5xa8Ubg.jpg" alt="Tacupra Update Post Image">
+			<p>A thick cast manifold was perfect for my application (Offroad - Exposure to water). SPAs manifold fit the bill. Get the SPA Manifold here:</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/FBWi942.jpg" alt="Tacupra Update Post Image">
-			<p>Some encouragement after pressing in the Camburg Upper arm spintle adapters.</p>
+			<img src="http://i.imgur.com/A6liXgZ.jpg" alt="Tacupra Update Post Image">
+			<p>A thick cast manifold was perfect for my application (Offroad - Exposure to water). SPAs manifold fit the bill.</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/KX48uLp.jpg" alt="Tacupra Update Post Image">
-			<p>In order to remove the upper control arm cross bolt you must remove the harness clamp from the inner fender and the airbag sensor. The inner fender does have to be pushed outwards to clear the bolt initially. The bolt does contact the coolant bottle. Applying forward facing torque on the control arm at the last moment will allow you to clear the coolant bottle.</p>
+			<img src="http://i.imgur.com/tBROpJn.jpg" alt="Tacupra Update Post Image">
+			<p>Needed a block off for the external wastegate provision in the SPA manifold. I used the EFR 7064 with an internal wastegate.</p>
+			<p>Vibrant perfomance block off plate: </p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/XJ872rX.jpg" alt="Tacupra Update Post Image">
-			<p>The upper control arm cross bolt on the passenger side. You will need to remove the clamp holding the A/C line from the inner fender and the air bag sensor.</p>
+			<img src="http://i.imgur.com/eOWhTqN.jpg" alt="Tacupra Update Post Image">
+			<p>A thick cast manifold was perfect for my application (Offroad - Exposure to water). SPAs manifold fit the bill.</p>
 		</div>
 	</div>
 </div>
@@ -65,41 +66,40 @@ upload-date: Uploaded June 10 2017
 	<div class="row">
 		<div class="content">
 			<img src="//i.imgur.com/S9FdPh8.jpg" alt="Tacupra Update Post Image">
-			<p>Mocking up the Camburg upper control arms. </p>
+			<p>Mock up of BorgWarner EFR 7064 and SPA Turbo Manifold.</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/kPPrd3F.jpg" alt="Tacupra Update Post Image">
-			<p>Close shot of the mock-up.</p>
+			<img src="http://i.imgur.com/rhNUAyK.jpg" alt="Tacupra Update Post Image">
+			<p>First test fit of the thin T4 Twinscroll flange before printing the full 3/4" version which would take several hours to print.</p>
+			<p>High quality ABS 3D printing filament:</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/bnzwzTW.jpg" alt="Tacupra Update Post Image">
-			<p>Cleaning the metal on the spindle for the Total Chaos spindle gusset. </p>
+			<img src="http://i.imgur.com/XfLH4zT.jpg" alt="Tacupra Update Post Image">
+			<p>I 3D printed a T4 Twinscroll flange (3/4") to represent the space the SP Quickspool valve will occupy between the turbo and the manifold.</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/3B9fH8r.jpg" alt="Tacupra Update Post Image">
-			<p>First time welding Mig uphill. <br>
-			See settings here: <a href="//imgur.com/9z3M5AR">http://imgur.com/9z3M5AR</a> <br>
-			I was running a generous amount of 75/25 Argon CO2</p>
+			<img src="http://i.imgur.com/0cOtbti.png" alt="Tacupra Update Post Image">
+			<p>I had help converting the EFR 7064 2D Drafts into 2D vectors (SVG). I then combined the 3D vectors in attempt to reconstruct a dimensionally accurate EFR 7064. You can see that I failed. Oh well, it would have been a disaster to 3D print on my 220mm X 220mm print bed.</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/hLk01as.jpg" alt="Tacupra Update Post Image">
-			<p>Prepping for paint.</p>
+			<img src="http://i.imgur.com/aE1lVQ1.png" alt="Tacupra Update Post Image">
+			<p>I had help converting the EFR 7064 2D Drafts into 2D vectors (SVG). I then combined the 3D vectors in attempt to reconstruct a dimensionally accurate EFR 7064. You can see that I failed. Oh well, it would have been a disaster to 3D print on my 220mm X 220mm print bed.</p>
 		</div>
 	</div>
 </div>
@@ -107,7 +107,7 @@ upload-date: Uploaded June 10 2017
 	<div class="row">
 		<div class="content">
 			<img src="//i.imgur.com/XfO1qIk.jpg" alt="Tacupra Update Post Image">
-			<p>Fox shocks Factory series with DSC. The key takeaway here is the DSC feature that allows you to adjust High Speed and Low Speed compression valve settings.</p>
+			<p>Doing a mockup of the 3D printed Quick Spool Valve.</p>
 		</div>
 	</div>
 </div>
@@ -122,17 +122,27 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/lUZomrF.jpg" alt="Tacupra Update Post Image">
-			<p>One off Deaver Leaf Springs - Specific to my application. <br>
-			Increase payload capacity 600 Lbs with minimal lift.</p>
+			<img src="http://i.imgur.com/GFMKFKD.jpg" alt="Tacupra Update Post Image">
+			<p>The 3/4" 3D printed model of the SP Quickspool valve confirms that the turbo will fit!</p>
+			<p>Using an internal wastegate BorgWarner EFR 7064 twin scroll:</p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="//i.imgur.com/H1kW5sv.jpg" alt="Tacupra Update Post Image">
-			<p>Increased travel!</p>
+			<img src="http://i.imgur.com/PJnMzyd.jpg" alt="Tacupra Update Post Image">
+			<p>2JZ GTE (Supra) in a 2001 4x4 Tacoma</p>
+			<p>The EFR 7064 turbo is fully supported by the sandwiched 3D printed model of the Quickspool valve and the associated hardware.</p>
+			<p>ARB bumper that will house the winch and reinforces the front end that I got for a great deal and free shipping! : https://www.amazon.com/gp/product/B004LQCG7E/ref=as_li_ss_tl?ie=UTF8&linkCode=ll1&tag=quatt01-20&linkId=1fddb663bed35f234ac26c9667e5bfd2</p>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<div class="content">
+			<img src="http://i.imgur.com/PJnMzyd.jpg" alt="Tacupra Update Post Image">
+			<p>CX Racing intercooler: </p>
 		</div>
 	</div>
 </div>
