fa0b6de20c3f18339eba930787db8f0ad363eb7c
Nico Watine <nico@studioncreations.com>
2017-09-11 17:17:27 -0500

    Remove specific “http” prefix for image links

--- src/pages/build-progress/single-turbo-fitment.php
+++ src/pages/build-progress/single-turbo-fitment.php
@@ -15,7 +15,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/9zJQdRw.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/9zJQdRw.jpg" alt="Tacupra Update Post Image">
 			<p>The R154 bellhousing was bolted to the R150F and the transmission crossmember was modified (pushing the full engine/trans assembly back 2-3.4"). The water pump pulley is VERY close to the radiator.</p>
 		</div>
 	</div>
@@ -32,15 +32,15 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/5xa8Ubg.jpg" alt="Tacupra Update Post Image">
-			<p>A thick cast manifold was perfect for my application (Offroad - Exposure to water). SPAs manifold fit the bill. <strong>Get the SPA Manifold here: <a href="http://www.ebay.com/itm/172337191151?rmvSB=true" target="_blank">ebay link</a>.</strong></p>
+			<img src="//i.imgur.com/5xa8Ubg.jpg" alt="Tacupra Update Post Image">
+			<p>A thick cast manifold was perfect for my application (Offroad - Exposure to water). SPAs manifold fit the bill. <strong>Get the SPA Manifold here: <a href="//www.ebay.com/itm/172337191151?rmvSB=true" target="_blank">ebay link</a>.</strong></p>
 		</div>
 	</div>
 </div>
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/A6liXgZ.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/A6liXgZ.jpg" alt="Tacupra Update Post Image">
 			<p>Another shot of the SPA Manifold.</p>
 		</div>
 	</div>
@@ -48,7 +48,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/eOWhTqN.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/eOWhTqN.jpg" alt="Tacupra Update Post Image">
 			<p>One more view of the SPA Manifold.</p>
 		</div>
 	</div>
@@ -56,7 +56,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/tBROpJn.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/tBROpJn.jpg" alt="Tacupra Update Post Image">
 			<p>Needed a block off for the external wastegate provision in the SPA manifold. I used the EFR 7064 with an internal wastegate.</p>
 			<p>Vibrant perfomance block off plate: </p>
 		</div>
@@ -73,7 +73,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/rhNUAyK.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/rhNUAyK.jpg" alt="Tacupra Update Post Image">
 			<p>First test fit of the thin T4 Twinscroll flange before printing the full 3/4" version which would take several hours to print.</p>
 			<p>High quality ABS 3D printing filament:</p>
 		</div>
@@ -82,7 +82,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/XfLH4zT.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/XfLH4zT.jpg" alt="Tacupra Update Post Image">
 			<p>I 3D printed a T4 Twinscroll flange (3/4") to represent the space the SP Quickspool valve will occupy between the turbo and the manifold.</p>
 		</div>
 	</div>
@@ -90,7 +90,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/0cOtbti.png" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/0cOtbti.png" alt="Tacupra Update Post Image">
 			<p>I had help converting the EFR 7064 2D Drafts into 2D vectors (SVG). I then combined the 3D vectors in attempt to reconstruct a dimensionally accurate EFR 7064. You can see that I failed. Oh well, it would have been a disaster to 3D print on my 220mm X 220mm print bed.</p>
 		</div>
 	</div>
@@ -98,7 +98,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/aE1lVQ1.png" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/aE1lVQ1.png" alt="Tacupra Update Post Image">
 			<p>I had help converting the EFR 7064 2D Drafts into 2D vectors (SVG). I then combined the 3D vectors in attempt to reconstruct a dimensionally accurate EFR 7064. You can see that I failed. Oh well, it would have been a disaster to 3D print on my 220mm X 220mm print bed.</p>
 		</div>
 	</div>
@@ -122,7 +122,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/GFMKFKD.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/GFMKFKD.jpg" alt="Tacupra Update Post Image">
 			<p>The 3/4" 3D printed model of the SP Quickspool valve confirms that the turbo will fit!</p>
 			<p>Using an internal wastegate BorgWarner EFR 7064 twin scroll:</p>
 		</div>
@@ -131,7 +131,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/PJnMzyd.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/PJnMzyd.jpg" alt="Tacupra Update Post Image">
 			<p>2JZ GTE (Supra) in a 2001 4x4 Tacoma</p>
 			<p>The EFR 7064 turbo is fully supported by the sandwiched 3D printed model of the Quickspool valve and the associated hardware.</p>
 			<p>ARB bumper that will house the winch and reinforces the front end that I got for a great deal and free shipping! : https://www.amazon.com/gp/product/B004LQCG7E/ref=as_li_ss_tl?ie=UTF8&linkCode=ll1&tag=quatt01-20&linkId=1fddb663bed35f234ac26c9667e5bfd2</p>
@@ -141,7 +141,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="http://i.imgur.com/PJnMzyd.jpg" alt="Tacupra Update Post Image">
+			<img src="//i.imgur.com/PJnMzyd.jpg" alt="Tacupra Update Post Image">
 			<p>CX Racing intercooler: </p>
 		</div>
 	</div>
