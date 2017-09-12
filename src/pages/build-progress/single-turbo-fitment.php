1909a05ffab66989c75d7730150b51dd910428aa
Nico Watine <nico@studioncreations.com>
2017-09-12 07:31:29 -0500

    Insert CDN variable before all img

--- src/pages/build-progress/single-turbo-fitment.php
+++ src/pages/build-progress/single-turbo-fitment.php
@@ -15,7 +15,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/bellhousing-waterpump.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/bellhousing-waterpump.jpg" alt="Tacupra Update Post Image">
 			<p>The R154 bellhousing was bolted to the R150F and the transmission crossmember was modified (pushing the full engine/trans assembly back 2-3.4"). The water pump pulley is VERY close to the radiator.</p>
 		</div>
 	</div>
@@ -32,7 +32,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/manifold-1.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/manifold-1.jpg" alt="Tacupra Update Post Image">
 			<p>A thick cast manifold was perfect for my application (Offroad - Exposure to water). SPAs manifold fit the bill.</p>
 			<p><strong>SPA T4 Manifold: <a href="//www.ebay.com/itm/172337191151?rmvSB=true" target="_blank">eBay Link</a></strong></p>
 		</div>
@@ -41,7 +41,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/manifold-2.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/manifold-2.jpg" alt="Tacupra Update Post Image">
 			<p>Another shot of the SPA Manifold.</p>
 		</div>
 	</div>
@@ -49,7 +49,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/manifold-3.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/manifold-3.jpg" alt="Tacupra Update Post Image">
 			<p>One more view of the SPA Manifold.</p>
 		</div>
 	</div>
@@ -57,7 +57,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/vibrant-performance-block-off-plate.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/vibrant-performance-block-off-plate.jpg" alt="Tacupra Update Post Image">
 			<p>Needed a block off for the external wastegate provision in the SPA manifold. I used the EFR 7064 with an internal wastegate.</p>
 			<p><strong>Vibrant Performance Block Off Plate: <a href="http://a.co/5iPRFVe" target="_blank">Amazon Link</a></strong></p>
 		</div>
@@ -74,7 +74,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/t4-twinscroll-flange.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/t4-twinscroll-flange.jpg" alt="Tacupra Update Post Image">
 			<p>First test fit of the thin T4 Twinscroll flange before printing the full 3/4" version which would take several hours to print.</p>
 			<p><strong>High Quality ABS 3D Printing Filament: <a href="http://a.co/7H1suXh" target="_blank">Amazon Link</a></strong></p>
 		</div>
@@ -83,7 +83,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/3d-printed-t4-twinscroll-flange.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/3d-printed-t4-twinscroll-flange.jpg" alt="Tacupra Update Post Image">
 			<p>I 3D printed a T4 Twinscroll flange (3/4") to represent the space the SP Quickspool valve will occupy between the turbo and the manifold.</p>
 		</div>
 	</div>
@@ -91,9 +91,9 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/efr-7064-3d-vector-1.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/efr-7064-3d-vector-1.jpg" alt="Tacupra Update Post Image">
 			<p>I had help converting the EFR 7064 2D Drafts into 2D vectors (SVG). I then combined the 3D vectors in attempt to reconstruct a dimensionally accurate EFR 7064. You can see that I failed. Oh well, it would have been a disaster to 3D print on my 220mm X 220mm print bed.</p>
-			<img src="/img/update-post/single-turbo-fitment/efr-7064-3d-vector-2.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/efr-7064-3d-vector-2.jpg" alt="Tacupra Update Post Image">
 		</div>
 	</div>
 </div>
@@ -108,7 +108,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/3d-printed-sp-quickspool-valve.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/3d-printed-sp-quickspool-valve.jpg" alt="Tacupra Update Post Image">
 			<p>The 3/4" 3D printed model of the SP Quickspool valve confirms that the turbo will fit!</p>
 			<p>Using an internal wastegate BorgWarner EFR 7064 twin scroll:</p>
 		</div>
@@ -117,7 +117,7 @@ upload-date: Uploaded June 10 2017
 <div class="container">
 	<div class="row">
 		<div class="content">
-			<img src="/img/update-post/single-turbo-fitment/arb-4x4-winch-pull-bar.jpg" alt="Tacupra Update Post Image">
+			<img src="{{ cdn }}/img/update-post/single-turbo-fitment/arb-4x4-winch-pull-bar.jpg" alt="Tacupra Update Post Image">
 			<p>2JZ GTE (Supra) in a 2001 4x4 Tacoma</p>
 			<p>The EFR 7064 turbo is fully supported by the sandwiched 3D printed model of the Quickspool valve and the associated hardware.</p>
 			<p>I chose a bumper from <strong>ARB 4x4 Accessories</strong> (<a href="http://arbusa.com" target="_blank">www.arbusa.com</a>) that will house the winch and reinforce the front end.</p>
