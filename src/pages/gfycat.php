8b434059553c183dddc308c9e8978e8cd120c7e4
Nico Watine <nico@studioncreations.com>
2017-09-16 13:33:28 -0500

    Zero effect with targeting via local css
    Try youtube embed for ie only

--- src/pages/gfycat.php
+++ src/pages/gfycat.php
@@ -9,3 +9,4 @@ body-id: gfycat
 body-class:
 layout: index-video-splash
 ---
+<div style='position:relative;padding-bottom:54%' id="gfycat-iframe"><iframe src='https://gfycat.com/ifr/OccasionalDecisiveKoi' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>
