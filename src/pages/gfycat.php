62bf740314dcb8fd4fff49d93fd4db9500f7a393
Nico Watine <nico@studioncreations.com>
2017-09-16 13:14:57 -0500

    New page to dev gfycat on ie11

---
+++ src/pages/gfycat.php
@@ -0,0 +1,11 @@
+---
+url: /gfycat
+title: Tacoma + Supra Engine Swap
+description: The official site of Team QuattroStig's Tacoma Supra Engine Swap - Toyota Tacupra!
+og-title: Toyota Tacoma + Supra Engine Swap
+og-description: The official site of Team QuattroStig's Tacoma Supra Engine Swap. Follow the build here - Go Tacupra!
+og-image: /img/og/og-image-1.jpg
+body-id: gfycat
+body-class:
+layout: index-video-splash
+---
