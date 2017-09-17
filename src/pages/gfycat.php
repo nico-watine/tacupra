db58be385cd7d7dc330a232834af9dd08236521e
Nico Watine <nico@studioncreations.com>
2017-09-17 12:18:02 -0500

    Add build-spec section to page to test scrolling

--- src/pages/gfycat.php
+++ src/pages/gfycat.php
@@ -18,3 +18,41 @@ layout: index-video-splash
 	<div class="gfycat-cover"></div>
 	<div style='position:relative;padding-bottom:54%' id="gfycat-iframe"><iframe src='https://gfycat.com/ifr/OccasionalDecisiveKoi' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>
 </div>
+<section class="build-specs">
+	<div class="row" style="margin-bottom: 20px;">
+		<div class="columns text-center">
+			<h1><strong>Build Specs</strong></h1>
+		</div>
+	</div>
+	<div class="row" data-equalizer>
+		<div class="medium-6 columns">
+			<div class="specCard" data-equalizer-watch>
+				<div class="specCard-header">
+					<h2><strong>Build Truck</strong></h2>
+					<div class="image-scrim">&nbsp;</div>
+					<img src="{{ cdn }}/img/index/build-specs/build-truck/tacoma.jpg" alt="Tacoma Build Truck" title="Tacoma Build Truck">
+				</div>
+				<div class="specCard-body">
+					<h4><strong>2001 Toyota Tacoma Xtra Cab</strong></h4>
+					<p>Manual Transmission <br>
+					4x4 Drivetrain <br>
+					Electronic Locking Rear Differential</p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-6 columns">
+			<div class="specCard" data-equalizer-watch>
+				<div class="specCard-header">
+					<h2><strong>Swap Engine</strong></h2>
+					<div class="image-scrim">&nbsp;</div>
+					<img src="{{ cdn }}/img/index/build-specs/swap-engine/supra-2-2.jpg" alt="Supra Swap Engine" title="Supra Swap Engine">
+				</div>
+				<div class="specCard-body">
+					<h4><strong>1998 Toyota Supra</strong></h4>
+					<p>2JZ-GTE Non-VVTi <br>
+					BW EFR 7064 w/ SP Quick Spool Valve</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
