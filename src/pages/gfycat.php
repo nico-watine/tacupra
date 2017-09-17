94a401597dff27c55ca29d44d327002683e24c62
Nico Watine <nico@studioncreations.com>
2017-09-17 16:35:25 -0500

    Add gfycat-cover the same id so style targeting is strong

--- src/pages/gfycat.php
+++ src/pages/gfycat.php
@@ -17,7 +17,7 @@ layout: index-video-splash
 	</div>
 	<div class="gfyitem" data-id="OccasionalDecisiveKoi"></div>
 	<div class="ie-only">
-		<div class="gfycat-cover"></div>
+		<div class="gfycat-cover" id="gfycat-cover"></div>
 		<div style='position:relative;padding-bottom:54%'><iframe src='https://gfycat.com/ifr/OccasionalDecisiveKoi' frameborder='0' scrolling='no' width='100%' height='100%' style='position:absolute;top:0;left:0' allowfullscreen></iframe></div>
 	</div>
 </div>
