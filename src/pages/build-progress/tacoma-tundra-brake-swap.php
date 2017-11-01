8d12daec26f946e2e8a12229aa4211715854e6f5
Nico Watine <nico@studioncreations.com>
2017-11-01 09:41:05 -0500

    Add new page to src and begin YAML properties

---
+++ src/pages/build-progress/tacoma-tundra-brake-swap.php
@@ -0,0 +1,14 @@
+---
+url: /build-progress/tacoma-tundra-brake-swap
+title: Tacoma Tundra Brake Swap
+description: I decided that the single turbo was my plan of action because the stock twins…
+og-title: Tacoma Tundra Brake Swap
+og-description: I decided that the single turbo was my plan of action because the stock twins… 
+og-image: /img/update-post/single-turbo-fitment/2jz-tacoma-single-turbo-fitment.png
+body-id: 
+body-class: update-post
+layout: updatePost
+hero-background-image: /img/update-post/tacoma-tundra-brake-swap/tacoma-tundra-brakes-swap.jpg
+post-title: Tacoma Tundra Brake Swap
+upload-date: October 28, 2017
+---
