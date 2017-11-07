92ad7d0c4a95fd7dfa92f3fa4d82b95526e4ebe4
Nico Watine <nico@studioncreations.com>
2017-11-07 01:22:36 -0600

    Caption styles tweaked for legibility

--- src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
+++ src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
@@ -94,14 +94,14 @@ upload-date: November 6, 2017
 	<h2 class="text-center" style="color: #fff;"><strong>Tool Setup</strong></h2>
 	<div class="row expanded collapse">
 		<div class="large-6 columns" style="position: relative;">
-			<div class="caption text-center padding-Y" style="background: rgba(0,0,0,0.50); position: absolute; bottom: 0; width: 100%;">
-				<p style="color: #fff; text-shadow: 0 2px 4px #000;" class="margin-0 xLarge">Gas settings for the MIG job</p>
+			<div class="caption text-center padding-Y" style="background: rgba(0,0,0,.7); position: absolute; bottom: 0; width: 100%;">
+				<p style="color: #fff;" class="margin-0 xLarge">Gas settings for the MIG job</p>
 			</div>
 			<img src="/img/update-post/4x4-2jz-tacoma-transmission-mount/gas-settings-mig-job.jpg" alt="">
 		</div>
 		<div class="large-6 columns" style="position: relative;">
-			<div class="caption text-center padding-Y" style="background: rgba(0,0,0,0.50); position: absolute; bottom: 0; width: 100%;">
-				<p style="color: #fff; text-shadow: 0 2px 4px #000;" class="margin-0 xLarge">MIG settings for the weld job</p>
+			<div class="caption text-center padding-Y" style="background: rgba(0,0,0,.7); position: absolute; bottom: 0; width: 100%;">
+				<p style="color: #fff;" class="margin-0 xLarge">MIG settings for the weld job</p>
 			</div>
 			<img src="/img/update-post/4x4-2jz-tacoma-transmission-mount/mig-settings-weld-job.jpg" alt="">
 		</div>
