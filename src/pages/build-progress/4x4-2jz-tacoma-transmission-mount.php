703869e2ea4da15e3fdd9a1c2b55b78040a98151
Nico Watine <nico@studioncreations.com>
2017-11-07 00:59:17 -0600

    Example card prose completed with span style

--- src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
+++ src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
@@ -69,7 +69,7 @@ upload-date: November 6, 2017
 	<div class="row" data-equalizer>
 		<div class="large-3 columns large-push-1">
 			<div class="measurementCard padding-XY" data-equalizer-watch>
-				<p>Front half of the shaft cut by <span>2.75”</span></p>
+				<p>Front half of the shaft cut by <span class="block"><strong>2.75”</strong></span></p>
 			</div>
 		</div>
 	</div>
