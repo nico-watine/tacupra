472cec1ccc3dec8ac7111f74060f8049cf173f59
Nico Watine <nico@studioncreations.com>
2017-11-06 20:59:44 -0600

    Being post YAML

--- src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
+++ src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
@@ -1,10 +1,14 @@
 ---
-url: 
-title: 
+url: /build-progress/4x4-2jz-tacoma-transmission-mount
+title: 4x4 2JZ Tacoma Transmission Mount
 description: 
-og-title: 
+og-title: 4x4 2JZ Tacoma Transmission Mount
 og-description: 
 og-image: 
-body-id: 
-body-class:
+body-id: tacoma-transmission-mount
+body-class: updatePost updatePost-unique
+layout: updatePost
+hero-background-image: /img/update-post/4x4-2jz-tacoma-transmission-mount/transmission-mount-clears.jpg
+post-title: 4x4 2JZ Tacoma Transmission Mount
+upload-date: November 6, 2017
 ---
