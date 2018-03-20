3cad9c994f63d84d18499004c7b031ed2ea4dddf
Nico Watine <nico@studioncreations.com>
2018-03-20 11:12:41 -0500

    Additional inline to class styling

--- src/pages/index.php
+++ src/pages/index.php
@@ -85,9 +85,7 @@ layout: index-video-splash
 <section class="instagramFeed">
 	<div class="row collapse">
 		<div class="medium-6 columns verbiage">
-			<div class="relative" style="height: 330px;">
-				<br>
-				<br>
+			<div class="relative heightWrap">
 				<div class="absolute prose padding-X">
 					<h1><strong>We post daily on Instagram.</strong></h1>
 					<h4>Follow <strong>@2jz_tacupra</strong> to keep up with the build status and see our latest expos.</h4>
