bac76ddc8144c8ad9ccba7c8f0cac4c49a5ff4c1
Nico Watine <nico@studioncreations.com>
2018-09-15 01:39:24 -0500

    Erase obsolte js lines

--- src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
+++ src/pages/build-progress/4x4-2jz-tacoma-transmission-mount.php
@@ -72,7 +72,7 @@ upload-date: November 6, 2017
 	<br>
 	<h2 class="text-center"><strong>Measurement Stats</strong></h2>
 	<br>
-	<div class="row" data-equalizer>
+	<div class="row">
 			<div class="large-4 columns large-push-1">
 			<div class="measurementCard margin-centered padding-XY">
 				<p class="margin-0">Initial measurement taken from middle of flange to front of carrier bearing bracket <span class="displayBlock"><strong>27.75”</strong></span></p>
