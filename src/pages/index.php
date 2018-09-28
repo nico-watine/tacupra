dea203aeb14238c70e31ac6c700507f8145c4d41
Nico Watine <nico@studioncreations.com>
2018-09-28 08:17:55 -0500

    Move possible classes from scss to utility class

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,13 +9,13 @@ body-id: index
 body-class:
 layout: index-video-splash
 ---
-<div class="index-video-splash">
+<div class="index-video-splash position-relative overflow-hidden">
 	<div class="row">
 		<div class="medium-10 medium-push-1 large-8 large-push-2 columns end logo-wrapper">
 			<img src="{{ cdn }}/img/logo/white-alpha-bg.png" alt="Toyota Tacupra Swap Logo" title="Toyota Tacupra Swap Logo">
 		</div>
 	</div>
-	<video autoplay muted loop poster="/img/video/timelapse-day-1/poster.jpg" id="backgroundVideo">
+	<video autoplay muted loop poster="/img/video/timelapse-day-1/poster.jpg" class="position-absolute" id="backgroundVideo">
 		<source src="/img/video/timelapse-day-1/tacupra-timelapse-day-1.webm" type="video/webm">
 		<source src="/img/video/timelapse-day-1/tacupra-timelapse-day-1.mp4" type="video/mp4">
 	</video>
