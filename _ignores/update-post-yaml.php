9092d88c6a941b071d451277b024c36f77a245b9
Nico Watine <nico@studioncreations.com>
2018-09-30 10:12:20 -0500

    Document template YAML for update posts

---
+++ _ignores/update-post-yaml.php
@@ -0,0 +1,14 @@
+---
+url:
+title:
+description:
+og-title:
+og-description:
+og-image:
+body-id: {potentially-page-url}
+body-class: {updatePost} {updatePost-stock / updatePost-unique}
+layout: {updatePost / updatePost-masonry}
+hero-background-image:
+post-title:
+upload-date: Uploaded Month XX, XXXX
+---
