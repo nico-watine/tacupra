897c745cc72db8ff5b31961097c4476690f7f085
Nico Watine <nico@studioncreations.com>
2021-04-09 23:08:22 -0500

    Import JS via codekit syntax and not app gui

--- js/src/masonry.js
+++ js/src/masonry.js
@@ -1,6 +1,5 @@
-// Prepend:
-	// masonry.js
-	// imagesloaded.js
+// @codekit-prepend "/bower_components/imagesloaded/imagesloaded.pkgd.min.js";
+// @codekit-prepend "/bower_components/masonry-layout/dist/masonry.pkgd.min.js";
 
 $('#masonryGrid').imagesLoaded( function() {
 	$('#masonryGrid').masonry({
