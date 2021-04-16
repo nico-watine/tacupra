e6270b46729966d66be4f39b30b6adf8bbcd5bd7
Nico Watine <nico@studioncreations.com>
2021-04-16 09:14:15 -0500

    Editing head YAML

---
+++ src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
@@ -0,0 +1,39 @@
+---
+url: /build-progress/making-2jz-4x4-tacoma-motor-mounts
+title: 2JZ 4x4 Tacoma Motor Mounts
+description:
+og-title: 2JZ 4x4 Tacoma Motor Mounts
+og-description:
+og-image:
+body-id: id-making-2jz-4x4-tacoma-motor-mounts
+body-class: updatePost updatePost-unique
+layout: updatePost-masonry
+hero-background-image:
+post-title: Making the 2JZ 4x4 Tacoma Motor Mounts
+upload-date: April 14, 2021
+---
+<section class="pageIntro-section">
+	<br>
+	<div class="row expanded collapse">
+		<div class="medium-6 medium-push-6 columns prose">
+			<div class="padding-X">
+				<h1><strong>Making the 2JZ 4x4 Tacoma Motor Mounts</strong></h1>
+				<p class="large"><strong>Since the Tacupra is receiving the 2JZ inline-6 engine from the Toyota Supra, I had to shorten the driveshaft.</strong></p>
+				<p>The 2JZ is longer than the Tacoma's original V6, so the make room, I cut the original driveshaft and pushed the transmission back. This meant I had to fabricate a whole new transmission crossmember.</p>
+				<br>
+				<span class="red-bar displayBlock"></span>
+				<br>
+				<br>
+			</div>
+		</div>
+		<div class="medium-6 medium-pull-6 columns">
+			<picture>
+				<source srcset="{{ cdn }}/img/update-post/4x4-2jz-tacoma-transmission-mount/transmission-mount-clears-fits-underneath.jpg" media="(min-width: 1020px)">
+				<source srcset="{{ cdn }}/img/update-post/4x4-2jz-tacoma-transmission-mount/800/transmission-mount-clears-fits-underneath.jpg" media="(min-width: 800px)">
+				<source srcset="{{ cdn }}/img/update-post/4x4-2jz-tacoma-transmission-mount/new-shortened-driveshaft.jpg" media="(min-width: 640px)">
+				<img src="{{ cdn }}/img/update-post/4x4-2jz-tacoma-transmission-mount/transmission-mount-clears-fits-underneath.jpg" alt="…">
+			</picture>
+		</div>
+	</div>
+	<br>
+</section>
