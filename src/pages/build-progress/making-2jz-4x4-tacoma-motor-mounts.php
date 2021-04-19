71b0ee43355c2a0777af91c05a68a0572c5be5e4
Nico Watine <nico@studioncreations.com>
2021-04-19 09:40:07 -0500

    Progressing using xy-grid

--- src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
+++ src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
@@ -33,10 +33,30 @@ upload-date: April 14, 2021
 	<br>
 </section>
 
+<section class="fab-pen-section grid-x align-middle" style="background: #fff;">
+	<div class="cell medium-6">
+		<div class="prose-wrap float-right mr-16 ml-40" style="max-width: 350px;">
+			<p>If you haven't used a paint pen for fab work, try it. I run through these so fast because I can use them for tons of different projects.</p>
+			<div class="amazon-link" style="background: #F1F1F1; border-radius: 4px;">
+				<img style="width:180px;" src="/img/assets/amazon/amazon-badge.svg" alt="">
+				<span style="font-size: 14px; width: 160px;" class="display-inline-block">SHARPIE MED WHT OB PAINT MARKER</span>
+			</div>
+		</div>
+	</div>
+	<div class="cell medium-6">
+		<img src="{{ cdn }}/img/update-post/making-2jz-4x4-tacoma-motor-mounts/_temp/fab-pen-video-thumbnail.png" alt="">
+	</div>
+</section>
+
+<br>
+
 <section class="fab-pen-section" style="background: #fff;">
 	<div class="row expanded collapse">
 		<div class="medium-6 columns">
 			<div class="fab-pen-prose float-right" style="max-width: 530px; padding-right: 16px;">
+				<div class="img-wrap float-left">
+					<img style="width: 50px;" src="/img/update-post/making-2jz-4x4-tacoma-motor-mounts/white-sharpie-paint-marker.jpg" alt="">
+				</div>
 				<div class="prose-wrap" style="padding-top: 100px; max-width: 350px;">
 					<p>If you haven't used a paint pen for fab work, try it. I run through these so fast because I can use them for tons of different projects.</p>
 					<div class="amazon-link" style="background: #F1F1F1; border-radius: 4px;">
