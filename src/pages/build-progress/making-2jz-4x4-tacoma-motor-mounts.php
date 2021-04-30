a4066f97391ce1a38191c6ab2c4d3cea4b6ec61d
Nico Watine <nico@studioncreations.com>
2021-04-30 12:27:10 -0500

    Autoplay video again

--- src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
+++ src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
@@ -50,7 +50,7 @@ upload-date: April 14, 2021
 			</div>
 		</div>
 		<div class="fab-pen-media columns">
-			<video playsinline  muted loop poster="{{ cdn }}/img/update-post/making-2jz-4x4-tacoma-motor-mounts/_temp/fab-pen-video-thumbnail.png">
+			<video playsinline autoplay muted loop poster="{{ cdn }}/img/update-post/making-2jz-4x4-tacoma-motor-mounts/_temp/fab-pen-video-thumbnail.png">
 				<source src="{{ cdn }}/img/update-post/making-2jz-4x4-tacoma-motor-mounts/_temp/fab-pen-video.mp4" type="video/mp4">
 				<!-- <source src="video.webm" type="video/webm"> -->
 				<!-- <source src="video.ogv" type="video/ogg"> -->
