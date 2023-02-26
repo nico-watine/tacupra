10efb30cbb029d047cd0a8a7144b8e908fd1dd28
Nico Watine <nico@studioncreations.com>
2023-02-26 13:09:13 -0600

    Column sizing tweaks

--- src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
+++ src/pages/build-progress/making-2jz-4x4-tacoma-motor-mounts.php
@@ -131,7 +131,7 @@ upload-date: April 14, 2021
 								<!-- style="max-width: 240px;" -->
 								<p class="text-white lead text-right mr-16 float-right">The first trimmed bolt, flush with the flange. The other 3 protruding bolts show how much longer they originally were.</p>
 							</div>
-							<div class="cell large-6">
+							<div class="cell medium-10 medium-offset-2 large-offset-0 large-6">
 								<div class="detail-shot position-relative">
 									<p class="position-absolute hide-for-large text-white p-12 m-0 w-100">The first trimmed bolt, flush with the flange. The other 3 protruding bolts show how much longer they originally were.</p>
 									<img src="/img/update-post/making-2jz-4x4-tacoma-motor-mounts/first-trimmed-bolt.jpg" alt="">
@@ -141,7 +141,7 @@ upload-date: April 14, 2021
 					</div>
 					<div class="cell medium-6">
 						<div class="grid-x align-middle">
-							<div class="cell large-6">
+							<div class="cell medium-10 large-6">
 								<div class="detail-shot position-relative">
 									<p class="position-absolute hide-for-large text-white p-12 m-0 w-100">Side-by-side comparison.</p>
 									<img src="/img/update-post/making-2jz-4x4-tacoma-motor-mounts/bolt-comparison.jpg" alt="">
